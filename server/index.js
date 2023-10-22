4da6290634c8ed9e5713be961f89ea207e5798b6
Anil Rai <anilrai4001@gmail.com>
2023-10-22 17:09:30 +0530

    added database config and initialzation

--- server/index.js
+++ server/index.js
@@ -1,17 +1,31 @@
 const express = require('express')
+const app = express();
+
+const mongoose = require('mongoose')
 const bodyparser = require('body-parser');
 const dotenv = require('dotenv')
 dotenv.config();
 
-app.use(express.bodyparser({extended:true}));
 
-const app = express();
+const PORT = process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_URL;
+
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
 })
 
-app.listen(process.env.PORT,(err)=>{
-    if(err) return console.log(error);
-    console.log('server started successfully at http://localhost:5000')
+app.get('/api/health',(req,res)=>{
+    res.json({
+        status:'active',
+        service:'job listing backend',
+        time: new Date()
+    })
+})
+
+app.listen(PORT,(err)=>{
+    mongoose
+        .connect(MONGODB_URL)
+        .then(()=>console.log(`server started successfully at http://localhost:${PORT}`))
+        .catch((err)=>console.log(err))
 })
