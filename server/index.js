a1fa101f3f89fa4a755b2efe97d4c3dadcd9aa2c
Anil Rai <anilrai4001@gmail.com>
2023-11-08 15:18:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv')
